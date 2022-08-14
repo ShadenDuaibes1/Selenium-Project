c11ded46d5d84d91a0ab8c7af21cb5d1fb37d7f5
shaden <shadeen20188@outlook.com>
2022-08-14 14:49:52 +0300

    First Commit-Added Project

---
+++ Template/src/PackageT/FirstClass.java
@@ -0,0 +1,17 @@
+package PackageT;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\auto\\chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+        driver.get("https://www.itgsoftware.com/");
+        System.out.println("Successfully opened");
+        driver.quit();
+	}
+
+}
